cce37c4a2eeee149ccd9bffc2852476751a624aa
jsmith-cb <jeremy.g.smith13@gmail.com>
2026-09-13 12:32:16 +0200

    Add employee planning page shell

---
+++ src/pages/EmployeePlanningPage.ts
@@ -0,0 +1,13 @@
+export function renderEmployeePlanningPage(
+    root: HTMLElement,
+): void {
+    root.innerHTML = `
+        <h1 class="page-heading">
+            Employee Planning
+        </h1>
+
+        <p class="page-placeholder">
+            Individual employee planning will be available here.
+        </p>
+    `;
+}
